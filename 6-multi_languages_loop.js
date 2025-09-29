1c8470c7c956272977533d723023357db3f69c76
oyinlola dominic <oyinloladominic@gmail.comgit initgit config --global user.email oyinloladominic@gmail.com>
2025-09-29 09:51:16 +0100

    Add script to print languages using a loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Array of languages
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array and print each line
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
